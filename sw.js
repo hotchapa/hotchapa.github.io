0ce5738a028fb0098ad0027e478ee24f894e1e3a
jihun <hotchapa@naver.com>
2024-03-30 00:00:37 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-de157c6b30d4790e4e81.js"
   },
   {
-    "url": "app-ff86377302a269900829.js"
+    "url": "app-e1130f5ee6e50d6cff9f.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6afdeb870b9b5f27322ebd50a7398420"
+    "revision": "4719bbb94fa12b234327ee826400ec54"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -166,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ff86377302a269900829.js`))) {
+  if (!resources || !(await caches.match(`/app-e1130f5ee6e50d6cff9f.js`))) {
     return await fetch(event.request)
   }
 
