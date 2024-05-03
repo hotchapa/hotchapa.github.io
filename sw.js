e9bc7965cf2ab742fa5378c954ea6d71af52a5e1
jihun <hotchapa@naver.com>
2024-05-03 18:18:35 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6d26f8241d531e8ab227.js"
+    "url": "webpack-runtime-c565c513aa054279dc41.js"
   },
   {
-    "url": "styles.f4de4ac227c54b47f53f.css"
+    "url": "styles.8a8feb6d36f4f95b3d76.css"
   },
   {
     "url": "framework-de157c6b30d4790e4e81.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e5c430cdf4578517e55acae81b61caac"
+    "revision": "4d125cec6173dcf193a249b1428515e1"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
