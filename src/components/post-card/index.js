fb3471952971c7d8eee6ed103beda4a4ea308dff
jihun <hotchapa@naver.com>
2024-05-03 19:54:57 +0900

    Fix : 이모지 크기 고정

--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -1,3 +1,32 @@
+// import { Link } from 'gatsby';
+// import React from 'react';
+// import './style.scss';
+
+// function PostCard({ post }) {
+//   const { id, slug, title, excerpt, date, categories, emoji} = post;
+//   return (
+//     <div className="post-card-wrapper">
+//       <Link className="post-card" key={id} to={slug}>
+//         <div className="emoji">{emoji}</div>
+//         <div className="title">{title}</div>
+//         <div className="date">{date}</div>
+//         {/* <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
+//         <div className="info"> */}
+//           {/* <div className="date">{date}</div> */}
+//           {/* <div className="categories">
+//             {categories.map((category) => (
+//               <Link className="category" key={category} to={`/posts/${category}`}>
+//                 {category}
+//               </Link>
+//             ))}
+//           </div> */}
+//         {/* </div> */}
+//       </Link>
+//     </div>
+//   );
+// }
+
+// export default PostCard;
 import { Link } from 'gatsby';
 import React from 'react';
 import './style.scss';
@@ -7,20 +36,13 @@ function PostCard({ post }) {
   return (
     <div className="post-card-wrapper">
       <Link className="post-card" key={id} to={slug}>
-        <div className="emoji">{emoji}</div>
-        <div className="title">{title}</div>
-        <div className="date">{date}</div>
-        {/* <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
-        <div className="info"> */}
-          {/* <div className="date">{date}</div> */}
-          {/* <div className="categories">
-            {categories.map((category) => (
-              <Link className="category" key={category} to={`/posts/${category}`}>
-                {category}
-              </Link>
-            ))}
-          </div> */}
-        {/* </div> */}
+        <div className="content-wrapper">
+          <div className="emoji">{emoji}</div>
+          <div className="content">
+            <div className="title">{title}</div>
+            <div className="date">{date}</div>
+          </div>
+        </div>
       </Link>
     </div>
   );
