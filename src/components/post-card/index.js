e74e477160058c000fc7da3c23bde9aeefe0c0e4
jihun <hotchapa@naver.com>
2024-05-03 18:02:22 +0900

    Fix : console log 삭제

--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -4,7 +4,6 @@ import './style.scss';
 
 function PostCard({ post }) {
   const { id, slug, title, excerpt, date, categories, emoji} = post;
-  console.log(post)
   return (
     <div className="post-card-wrapper">
       <Link className="post-card" key={id} to={slug}>
