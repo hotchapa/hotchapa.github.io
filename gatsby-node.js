6bccc2a38b2de82f1cc37d0af68f99f090f46398
jihun <hotchapa@naver.com>
2024-07-23 14:43:34 +0900

    Core :  GraphQL 스키마 정의

--- gatsby-node.js
+++ gatsby-node.js
@@ -102,3 +102,73 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   createBlogPages({ createPage, results });
   createPostsPages({ createPage, results });
 };
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  createTypes(`
+    type SiteSiteMetadata {
+      title: String
+      description: String
+      language: String
+      siteUrl: String
+      ogImage: String
+      comments: Comments
+      ga: String
+      author: Author
+      about: About
+    }
+
+    type Comments {
+      utterances: Utterances
+    }
+
+    type Utterances {
+      repo: String
+    }
+
+    type Author {
+      name: String
+      bio: AuthorBio
+      social: AuthorSocial
+    }
+
+    type AuthorBio {
+      role: String
+      description: [String]
+      thumbnail: String
+    }
+
+    type AuthorSocial {
+      github: String
+      linkedIn: String
+      email: String
+    }
+
+    type About {
+      timestamps: [Timestamp]
+      projects: [Project]
+    }
+
+    type Timestamp {
+      date: String
+      activity: String
+      links: Links
+    }
+
+    type Project {
+      title: String
+      description: String
+      techStack: [String]
+      thumbnailUrl: String
+      links: Links
+    }
+
+    type Links {
+      github: String
+      post: String
+      googlePlay: String
+      appStore: String
+      demo: String
+    }
+  `);
+};
